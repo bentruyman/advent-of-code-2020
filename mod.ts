a763a3e4fadacbd6f64485d1011b1747c24977ee
Ben Truyman <ben@truyman.com>
2021-12-05 16:49:45 -0800

    feat: add initial repo structure

---
+++ mod.ts
@@ -0,0 +1,33 @@
+type SolutionPart = (input: string) => number;
+type Solution = {
+  part1: SolutionPart;
+  part2: SolutionPart;
+};
+
+async function exec(day: number): Promise<void> {
+  const solution = solutions[day - 1];
+
+  if (solution === undefined)
+    throw new Error(`No solution found for day: ${day}`);
+
+  const input = await readInput(day);
+  console.log(`Day ${day}:`, solution.part1(input), solution.part2(input));
+}
+
+async function readInput(day: number): Promise<string> {
+  const decoder = new TextDecoder();
+  const data = await Deno.readFile(`./data/day${day}.txt`);
+
+  return decoder.decode(data).trimEnd();
+}
+
+const day = parseInt(Deno.args[0]);
+const solutions: Solution[] = [];
+
+if (isNaN(day)) {
+  for (let index = 1; index <= solutions.length; index++) {
+    await exec(index);
+  }
+} else {
+  await exec(day);
+}
